d674a3b97241d695718abed9948f5875703155f8
Bl4omArchie <grlch.alexandre@proton.me>
2024-10-17 17:53:59 +0200

    more prototypes

--- includes/rsa.h
+++ includes/rsa.h
@@ -6,6 +6,7 @@
 #include <openssl/bn.h>
 
 
+
 typedef struct _rsa_keypair {
     int key_bit_size;            // key size in bit : only 2048, 3072 and 8196 are accepted
     BIGNUM *public_modulus;      //n
@@ -23,6 +24,11 @@ int set_public_exp(Crypt_RSA *key, BIGNUM *e);
 int set_private_exp(Crypt_RSA *key, BIGNUM *d);
 int set_dQ(Crypt_RSA *key, BIGNUM *dQ);
 int set_qI(Crypt_RSA *key, BIGNUM *qI);
+
+Crypt_RSA *generate(int key_bit_size);
+int encrypt(Crypt_RSA *key, const char *pt, const char *ct, size_t pt_size, size_t ct_size);
+int decrypt(Crypt_RSA *key, const char *ct, const char *pt, size_t ct_size, size_t pt_size);
+
 
 
 
